fe7ab1538f8825440700afdf804c5d5981ec0b4d
Daniel1702 <106408440+Daniel1702@users.noreply.github.com>
2022-06-02 22:19:38 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+__author__ = "Daniel de Andrade"
+print("----Somatória de Dois Valores----")
+a = int(input("Insira o primeiro valor: "))
+b = int(input("Insira o segundo valor: "))
+
+soma = a+b
+c = soma 
+
+print("\nA Soma dos valores informados ({} + {}) é {}".format(a,b,c)) 
